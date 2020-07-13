dde3774c79dab222ed2bb91cbd13afff904b70dc
nunzio <nunzio03@hotmail.it>
2020-07-13 14:13:26 +0200

    measuring horizontal and vertical distance

--- pose_estimation.py
+++ pose_estimation.py
@@ -13,8 +13,8 @@ SLOWDONW_THRESHOLD_Z = 15
 DRONE_SPEED_X = 20
 DRONE_SPEED_Y = 15
 DRONE_SPEED_Z = 20
-SET_POINT_X = 960/2
-SET_POINT_Y = 720/2
+SET_POINT_X = 960 / 2
+SET_POINT_Y = 720 / 2
 SET_POINT_Z = 75
 
 aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
@@ -22,62 +22,59 @@ parameters = aruco.DetectorParameters_create()
 
 # video source and calibration parameters setup
 
-#video_capture = cv2.VideoCapture(0)
+# video_capture = cv2.VideoCapture(0)
 drone = Tello()
 drone.connect()
 print(drone.get_battery())
 drone.streamon()
 
-
 pc_mtx = np.array([[1.73223258e+03, 0.00000000e+00, 1.27300230e+03],
-       [0.00000000e+00, 1.73223258e+03, 1.03042217e+03],
-       [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+                   [0.00000000e+00, 1.73223258e+03, 1.03042217e+03],
+                   [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 
 pc_dist = np.array([[-3.89477169e+00],
-       [-9.72135202e-02],
-       [ 1.04299558e-02],
-       [ 8.40170760e-05],
-       [-2.17736443e+00],
-       [-3.89139953e+00],
-       [-1.90794581e-01],
-       [-1.85298591e+00],
-       [ 0.00000000e+00],
-       [ 0.00000000e+00],
-       [ 0.00000000e+00],
-       [ 0.00000000e+00],
-       [ 0.00000000e+00],
-       [ 0.00000000e+00]])
+                    [-9.72135202e-02],
+                    [1.04299558e-02],
+                    [8.40170760e-05],
+                    [-2.17736443e+00],
+                    [-3.89139953e+00],
+                    [-1.90794581e-01],
+                    [-1.85298591e+00],
+                    [0.00000000e+00],
+                    [0.00000000e+00],
+                    [0.00000000e+00],
+                    [0.00000000e+00],
+                    [0.00000000e+00],
+                    [0.00000000e+00]])
 
 drone_mtx = np.array([[1.74213359e+03, 0.00000000e+00, 1.27150514e+03],
- [0.00000000e+00, 1.74213359e+03, 1.02516982e+03],
- [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+                      [0.00000000e+00, 1.74213359e+03, 1.02516982e+03],
+                      [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 
 drone_dist = np.array([[-1.69684883e+00],
- [-6.85717812e+00],
- [ 9.93624014e-03],
- [ 6.20144084e-04],
- [-1.18739065e+01],
- [-1.69460711e+00],
- [-6.99110211e+00],
- [-1.13633464e+01],
- [ 0.00000000e+00],
- [ 0.00000000e+00],
- [ 0.00000000e+00],
- [ 0.00000000e+00],
- [ 0.00000000e+00],
- [ 0.00000000e+00]])
-
+                       [-6.85717812e+00],
+                       [9.93624014e-03],
+                       [6.20144084e-04],
+                       [-1.18739065e+01],
+                       [-1.69460711e+00],
+                       [-6.99110211e+00],
+                       [-1.13633464e+01],
+                       [0.00000000e+00],
+                       [0.00000000e+00],
+                       [0.00000000e+00],
+                       [0.00000000e+00],
+                       [0.00000000e+00],
+                       [0.00000000e+00]])
 
 mtx, dist = drone_mtx, drone_dist
 
 # loop start
-#drone.takeoff()
+drone.takeoff()
 while True:
 
-    #ret, frame = video_capture.read()
+    # ret, frame = video_capture.read()
 
     frame = drone.get_frame_read().frame
-
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -86,12 +83,12 @@ while True:
     # SUB PIXEL DETECTION
     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.0001)
     for corner in corners:
-        cv2.cornerSubPix(gray, corner, winSize = (3,3), zeroZone = (-1,-1), criteria = criteria)
+        cv2.cornerSubPix(gray, corner, winSize=(3, 3), zeroZone=(-1, -1), criteria=criteria)
 
     frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
 
     size_of_marker = 0.15  # side lenght of the marker in meters
-    rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(corners, size_of_marker , mtx, dist)
+    rvecs, tvecs, _objPoints = aruco.estimatePoseSingleMarkers(corners, size_of_marker, mtx, dist)
 
     length_of_axis = 0.1
     imaxis = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
@@ -100,9 +97,9 @@ while True:
         for i in range(len(tvecs)):
             imaxis = aruco.drawAxis(imaxis, mtx, dist, rvecs[i], tvecs[i], length_of_axis)
             dst, jacobian = cv2.Rodrigues(rvecs)
-            #print("phi:", math.atan2(dst[2][0], dst[2][1])*180/math.pi)
-            #print("theta:", math.acos(dst[2][2])*180/math.pi)
-            #print("psi:", -math.atan2(dst[0][2], dst[1][2])*180/math.pi)
+            # print("phi:", math.atan2(dst[2][0], dst[2][1])*180/math.pi)
+            # print("theta:", math.acos(dst[2][2])*180/math.pi)
+            # print("psi:", -math.atan2(dst[0][2], dst[1][2])*180/math.pi)
             x = (corners[0][0][0][0] + corners[0][0][2][0]) / 2
             y = (corners[0][0][0][1] + corners[0][0][2][1]) / 2
             square_side_dimension_px = math.sqrt(math.pow(corners[0][0][3][1] - corners[0][0][0][1], 2) +
@@ -114,10 +111,10 @@ while True:
 
         frontal_distance_cm = frontal_distance_cm_drone
         cm_pix_ratio = 15 / square_side_dimension_px
-        horizontal_distance_cm = (x-SET_POINT_X)*cm_pix_ratio
-        vertical_distance_cm = (y-SET_POINT_Y)*cm_pix_ratio
+        horizontal_distance_cm = -(x - SET_POINT_X) * cm_pix_ratio
+        vertical_distance_cm = -(y - SET_POINT_Y) * cm_pix_ratio
 
-        #imaxis = cv2.putText(imaxis, str(distance_cm), (100, 200), 5, 5, (250, 255, 250)) #distance frontal
+        # imaxis = cv2.putText(imaxis, str(distance_cm), (100, 200), 5, 5, (250, 255, 250)) #distance frontal
 
         imaxis = cv2.putText(imaxis, str(horizontal_distance_cm), (100, 200), 5, 5, (250, 255, 250))
 
@@ -128,8 +125,8 @@ while True:
         print("non vedo")
         x, y, frontal_distance_cm = None, None, None
 
-    cv2.circle(imaxis, (int(960/2), int(720/2)), 12, (0, 0, 255), 3)
-    x = None #debug
+    cv2.circle(imaxis, (int(960 / 2), int(720 / 2)), 12, (0, 0, 255), 3)
+
     if x is not None:
         error_x = x - SET_POINT_X
         error_y = y - SET_POINT_Y
@@ -177,12 +174,14 @@ while True:
 
     else:
         right_left_velocity, up_down_velocity, front_back_velocity = 0, 0, 0
+        vertical_distance_cm = 0
+        horizontal_distance_cm = 0
 
-    #drone.send_rc_control(0, front_back_velocity, up_down_velocity, right_left_velocity)
+    drone.send_rc_control(0, front_back_velocity, up_down_velocity, right_left_velocity)  # turn with yaw
+    drone.send_rc_control(right_left_velocity, front_back_velocity, up_down_velocity, 0)  # turn with roll
 
-
-    width = 2400/2
-    ratio = 16/9
+    width = 2400 / 2
+    ratio = 16 / 9
     dim = (int(width), int(width / ratio))
     imaxis = cv2.resize(imaxis, dim, interpolation=cv2.INTER_AREA)
     cv2.imshow("markers", imaxis)
@@ -191,4 +190,3 @@ while True:
         drone.land()
         drone.get_battery()
         break
-
