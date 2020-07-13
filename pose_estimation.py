147a4f94ef74e695abc5de900c71a1d7dad1cb23
nunzio <nunzio03@hotmail.it>
2020-07-13 15:17:17 +0200

    Merge branch 'proportional' of D:\Desktop dei DATI\Cartelle OneDrive\OneDrive - Politecnico di Bari\Programmazione\Python\follow-me-drone with conflicts.

--- pose_estimation.py
+++ pose_estimation.py
@@ -127,9 +127,9 @@ while True:
 
     cv2.circle(imaxis, (int(960 / 2), int(720 / 2)), 12, (0, 0, 255), 3)
 
-     drone.send_rc_control(0, front_back_velocity, up_down_velocity, right_left_velocity)  # turn with yaw
+    drone.send_rc_control(0, front_back_velocity, up_down_velocity, right_left_velocity)  # turn with yaw
     # drone.send_rc_control(right_left_velocity, front_back_velocity, up_down_velocity, 0)  # turn with roll
- # test proport
+
     #frontal_error = frontal_distance_cm - SET_POINT_Z_cm
 
     #drone.go_xyz_speed(frontal_distance_cm, horizontal_distance_cm, vertical_distance_cm, 30)
